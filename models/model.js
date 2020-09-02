cbfdeae7e518fef1e3bcf9c876be9a98abe76128
Wale Timothy <awtim01@gmail.com>
2020-09-02 21:50:20 +0100

    added body parser

--- models/model.js
+++ models/model.js
@@ -5,8 +5,6 @@ let userSchema = new Schema({
     name: { type: String, required: true, max: 100 },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    phone: { type: String, required: true },
-    google_token: { type: String }
 });
 
 let moviesSchema = new Schema({
--- server.js
+++ server.js
@@ -3,28 +3,18 @@ const app = express();
 const config = require('./config');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyparser = require('body-parser');
 const routes = require('./routes/route');
 const authController = require('./app/authController');
 const moviesController = require('./app/moviesController');
 const validateToken = require('./app/middleware').checkToken;
 
-// app.get('/', (req, res) => res.send('Hello World!'));
-// Users Routes
-app.get('/', authController.test);
-app.post('/register', authController.register);
-app.post('/login', authController.login);
-app.get('/user/:id', authController.getUser);
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 
-// Plans routes
-app.get('movies', validateToken, moviesController.getMovies);
-app.post('movie/add', validateToken, moviesController.newMovie);
-app.put('/movie/edit/:id', validateToken, moviesController.editMovie);
-app.delete('/movie/delete/:id', validateToken, moviesController.deleteMovie);
-// app.use('',routes);
+app.use(routes);
 
 app.use(cors);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect(config.db_url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
   if (err) {
